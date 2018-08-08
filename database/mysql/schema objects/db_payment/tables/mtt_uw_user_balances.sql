51eefed8884a0c741d2f871996cd492cb34318f3
hyoungsookim <hskim@groceryheaven.com>
2018-08-07 19:41:17 -0700

    Change type from varchar(10) to smallint

--- database/mysql/schema objects/db_payment/tables/mtt_uw_user_balances.sql
+++ database/mysql/schema objects/db_payment/tables/mtt_uw_user_balances.sql
@@ -1,12 +1,10 @@
 /*
     User Balances
-
-    balance_type: 'MILEAGE', 'CASH'
 */
 create table mtt_uw_user_balances
 (
     user_id         varchar(50)         not null,
-    balance_type    varchar(10)         not null,
+    balance_type    smallint            not null,
     balance_amount  integer             not null,
     updated_at      datetime            not null,
 
