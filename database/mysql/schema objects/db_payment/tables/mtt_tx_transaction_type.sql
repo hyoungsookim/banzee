ac5cf70c18d5b1473f9cbef80f686df39b311749
hyoungsookim <hskim@groceryheaven.com>
2018-08-13 18:42:07 -0700

    Add trx_type_description colummn

--- database/mysql/schema objects/db_payment/tables/mtt_tx_transaction_type.sql
+++ database/mysql/schema objects/db_payment/tables/mtt_tx_transaction_type.sql
@@ -3,10 +3,11 @@
 */
 create table mtt_tx_transaction_types
 (
-    trx_type        smallint            not null,
-    trx_type_name   varchar(50)         not null,
-    created_at      datetime            not null,
-    updated_at      datetime            not null,
+    trx_type                smallint            not null,
+    trx_type_name           varchar(50)         not null,
+    created_at              datetime            not null,
+    updated_at              datetime            not null,
+    trx_type_description    varchar(250)        null,
 
     constraint pk_mtt_tx_transaction_types
         primary key (trx_type)
