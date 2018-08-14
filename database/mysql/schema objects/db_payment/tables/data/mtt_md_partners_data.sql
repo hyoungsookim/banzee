40a37dece626d5e7b5d0091f886aed2c4eb7a937
hyoungsookim <hskim@groceryheaven.com>
2018-08-13 18:44:08 -0700

    Add initial data to mtt_md_partners

---
+++ database/mysql/schema objects/db_payment/tables/data/mtt_md_partners_data.sql
@@ -0,0 +1,8 @@
+/*
+    Partner data
+*/
+insert into mtt_md_partners 
+    (partner_id, partner_status, partner_name, created_at, updated_at) 
+values
+    ('VINCLE', 200, 'Vincle.me', current_timestamp(), current_timestamp());
+
