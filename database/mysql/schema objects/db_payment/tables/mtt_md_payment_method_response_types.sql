601ea6bd50b78a130b0a69cc8565a4cb956c61c1
hyoungsookim <hskim@groceryheaven.com>
2018-08-19 19:23:05 -0700

    Resize 2 columns of mtt_md_payment_method_response_types table

--- database/mysql/schema objects/db_payment/tables/mtt_md_payment_method_response_types.sql
+++ database/mysql/schema objects/db_payment/tables/mtt_md_payment_method_response_types.sql
@@ -4,11 +4,11 @@
 create table mtt_md_payment_method_response_types
 (
     method_code         varchar(10)         not null,
-    payment_response    varchar(20)         not null,
+    payment_response    varchar(50)         not null,
     payment_status      smallint            not null,
     created_at          datetime            not null,
     updated_at          datetime            not null,
-    type_description    varchar(250)        null,
+    type_description    text                null,
 
     constraint pk_mtt_md_payment_method_response_types
         primary key (method_code, payment_response, payment_status)
