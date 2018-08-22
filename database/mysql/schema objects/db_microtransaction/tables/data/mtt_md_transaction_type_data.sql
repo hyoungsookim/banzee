57663faaa46ead1b0f6d320d181b311626a6621b
hyoungsookim <hskim@groceryheaven.com>
2018-08-22 00:51:20 -0700

    Rename database from db_payment to db_microtransaction

---
+++ database/mysql/schema objects/db_microtransaction/tables/data/mtt_md_transaction_type_data.sql
@@ -0,0 +1,8 @@
+/*
+    Trnasaction type data
+*/
+insert into mtt_tx_transaction_types 
+    (trx_type, trx_type_name, created_at, updated_at)
+values
+    (0, '', current_timestamp(), current_timestamp()),
+    (0, '', current_timestamp(), current_timestamp());
