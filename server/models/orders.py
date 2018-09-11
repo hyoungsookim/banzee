f89ea9818e5bde21cc7a1c869c4c607e46df1e54
hyoungsookim <hskim@groceryheaven.com>
2018-09-10 22:33:01 -0700

    Change nullable options

--- server/models/orders.py
+++ server/models/orders.py
@@ -11,7 +11,7 @@ class Order(Base):
     __tablename__ = "mtt_tx_orders"
 
     order_no        = Column(Integer, primary_key=True)
-    order_id        = Column(String(50), nullable=False)
+    order_id        = Column(String(50), nullable=False, unique=True)
     user_no         = Column(Integer, nullable=False)
     order_status    = Column(SmallInteger, nullable=False)
     created_at      = Column(DateTime, nullable=False)
@@ -19,8 +19,8 @@ class Order(Base):
     order_amount    = Column(Numeric(12, 2), nullable=False)
     tax_amount      = Column(Numeric(12, 2), nullable=False)
     total_amount    = Column(Numeric(12, 2), nullable=False)
-    platform_type   = Column(String(50), nullable=False)
-    app_type        = Column(String(50), nullable=False)
+    platform_type   = Column(String(50), nullable=True)
+    app_type        = Column(String(50), nullable=True)
 
     def __init__(self, order_id, user_no, order_status, 
                 order_amount, tax_amount, total_amount, 
