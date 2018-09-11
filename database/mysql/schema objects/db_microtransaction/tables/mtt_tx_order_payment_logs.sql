aa9739240cc2c029509fd573c144da7fabff6f7c
hyoungsookim <hskim@groceryheaven.com>
2018-09-10 19:32:19 -0700

    Add payment_no column as a new primary key

--- database/mysql/schema objects/db_microtransaction/tables/mtt_tx_order_payment_logs.sql
+++ database/mysql/schema objects/db_microtransaction/tables/mtt_tx_order_payment_logs.sql
@@ -4,8 +4,7 @@
 create table mtt_tx_order_payment_logs
 (
 	log_no 					int 			not null auto_increment,
-	order_no 				int 			not null,
-	method_code 			varchar(10) 	not null,
+	payment_no 				int 			not null,
 	payment_status 			smallint		not null,
 	created_at		        datetime		not null,
 	remote_ip	 			varchar(50)		null,
