4946990a04c53747aac919d2089219856ce5c43f
hyoungsookim <hskim@groceryheaven.com>
2018-09-10 19:34:21 -0700

    Chang not null contraints to null

--- database/mysql/schema objects/db_microtransaction/tables/mtt_tx_orders.sql
+++ database/mysql/schema objects/db_microtransaction/tables/mtt_tx_orders.sql
@@ -7,13 +7,13 @@ create table mtt_tx_orders
 	order_id 				varchar(50)		not null,
 	user_no 				int 			not null,
 	order_status 			smallint		not null,
-	platform_type			varchar(50)		not null,
-	app_type				varchar(50)		not null,
 	created_at		        datetime 		not null,
 	updated_at 		        datetime 		not null,
 	order_amount 			decimal(12, 2) 	not null,
 	tax_amount 				decimal(12, 2)	not null,
 	total_amount 			decimal(12, 2)	not null,
+	platform_type			varchar(50)		null,
+	app_type				varchar(50)		null,
 
     constraint pk_mtt_tx_orders
 		primary key (order_no)
