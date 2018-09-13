80d5aac41033e731f70fa9a51c7e21dace268e9b
hyoungsookim <hskim@groceryheaven.com>
2018-09-12 22:19:55 -0700

    Add a user for localhost

--- database/mysql/schema objects/db_microtransaction/users/banzee.sql
+++ database/mysql/schema objects/db_microtransaction/users/banzee.sql
@@ -3,3 +3,6 @@ use mysql;
 /* banzee is a user for API */
 grant select, insert, update, delete, create temporary tables, execute on db_microtransaction.* to 'banzee'@'%';
 flush privileges;
+
+grant select, insert, update, delete, create temporary tables, execute on db_microtransaction.* to 'banzee'@'localhost';
+flush privileges;
