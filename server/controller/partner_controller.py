ba7fd9c32404d9fdf8c5a7d0dfec108a67f539f6
hyoungsookim <hskim@groceryheaven.com>
2018-09-27 22:08:53 -0700

    Implement controllers

--- server/controller/partner_controller.py
+++ server/controller/partner_controller.py
@@ -1,27 +1,31 @@
 """
 """
 
+import simplejson as json
+
 from server.utils import *
 from server.models.partner import Partner
 from server.data.partner_data import PartnerData
 
 
 class PartnerController(object):
+    partnerData = PartnerData()
 
     def __init__(self):
         pass
 
     def get_list(self, q, offset=0, fetch=20):
-        pass
+        return self.partnerData.get_list(None, offset, fetch)
     
     def get(self, partner_id):
-        pass
+        return json.dumps(self.partnerData.get(partner_id), 
+                sort_keys=False, encoding="utf-8", use_decimal=True, indent=4, default=str)
 
     def create(self, partner):
-        pass
+        return self.partnerData.create(partner)
 
     def update(self, partner):
-        pass
+        return self.partnerData.update(partner)
 
     def delete(self, partner_id):
-        pass
+        return self.partnerData.delete(partner_id)
