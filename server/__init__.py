e5356ee907e468224f23148aeb0482067cb79868
hyoungsookim <hskim@groceryheaven.com>
2018-10-04 22:01:02 -0700

    Add a blueprint for product_resource

--- server/__init__.py
+++ server/__init__.py
@@ -21,11 +21,12 @@ app.config['SQLALCHEMY_ECHO'] = False
 from server.api.v1.system_resource import system_resource
 from server.api.v1.partner_resource import partner_resource
 from server.api.v1.payment_method_resource import payment_method_resource
+from server.api.v1.product_resource import product_resource
 
 app.register_blueprint(system_resource)
 app.register_blueprint(partner_resource)
 app.register_blueprint(payment_method_resource)
-
+app.register_blueprint(product_resource)
 
 #db = SQLAlchemy(app)
 
