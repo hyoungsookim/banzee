75848f0731c420f2b87853c9ebcec783fa46204a
hyoungsookim <hskim@groceryheaven.com>
2018-10-30 20:10:49 -0700

    Add mysql new_id function to generate sequential uuid value

---
+++ database/mysql/schema objects/db_microtransaction/functions/new_id.sql
@@ -0,0 +1,18 @@
+DELIMITER //
+CREATE DEFINER=`root`@`%` FUNCTION `new_id`()
+RETURNS char(36) CHARSET utf8
+    NO SQL
+    DETERMINISTIC
+begin
+	declare $id char(36);
+
+    set $id = uuid();
+
+    return concat(substr($id, 15, 4), '-', 
+                  substr($id, 10, 4), '-', 
+                  substr($id, 1, 8), '-', 
+                  substr($id, 20, 4), '-', 
+                  substr($id, 25));
+end;
+//
+DELIMITER ;
