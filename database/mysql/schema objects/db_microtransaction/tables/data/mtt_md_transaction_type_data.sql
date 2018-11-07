1e788880950ce808343ac96cc8e5901477dbad0a
hyoungsookim <hskim@groceryheaven.com>
2018-11-07 00:42:00 -0800

    Add parent_trx_type column and load initial data

--- database/mysql/schema objects/db_microtransaction/tables/data/mtt_md_transaction_type_data.sql
+++ database/mysql/schema objects/db_microtransaction/tables/data/mtt_md_transaction_type_data.sql
@@ -2,7 +2,11 @@
     Trnasaction type data
 */
 insert into mtt_tx_transaction_types 
-    (trx_type, trx_type_name, created_at, updated_at)
+    (trx_type, trx_type_name, io_type, parent_trx_type, created_at, updated_at)
 values
-    (0, '', current_timestamp(), current_timestamp()),
-    (0, '', current_timestamp(), current_timestamp());
+    (1000, 'Income',              1, null, current_timestamp(), current_timestamp()),
+    (1100, 'Reward',              1, 1000, current_timestamp(), current_timestamp()),
+    (1200, 'Cash',                1, 1000, current_timestamp(), current_timestamp()),
+    (2000, 'Expense',            -1, null, current_timestamp(), current_timestamp()),
+    (2100, 'Item purchasing',    -1, 2000, current_timestamp(), current_timestamp()),
+    (2200, 'Service purchasing', -1, 2000, current_timestamp(), current_timestamp());
--- database/mysql/schema objects/db_microtransaction/tables/mtt_tx_transaction_type.sql
+++ database/mysql/schema objects/db_microtransaction/tables/mtt_tx_transaction_type.sql
@@ -2,13 +2,14 @@
     Transaction Types
 
     io_type: 1 - input(deposit)
-            -1 - output(spent)
+            -1 - output(expense)
 */
 create table mtt_tx_transaction_types
 (
     trx_type                smallint            not null,
     trx_type_name           varchar(50)         not null,
     io_type                 tinyint             not null,
+    parent_trx_type         smallint            null,
     created_at              datetime            not null,
     updated_at              datetime            not null,
     trx_type_description    varchar(250)        null,
