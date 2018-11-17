6f56303b2b1d3b1de014cea64ed0d1a47fec1554
hyoungsookim <hskim@groceryheaven.com>
2018-11-16 21:47:11 -0800

    Add message 406 and 412

--- server/messages.py
+++ server/messages.py
@@ -9,12 +9,14 @@ messages = {
     200: "The request was completed successfully.",
     205: "The transaction is canceled."
     400: "Invalid request. Client might be missing some mandatory values or might call a wrong API.",
-    401: "API authentication failed. Invalid request-header field or not acceptable client.",
+    401: "Authentication failed. Invalid request-header field or not acceptable client.",
     402: "Payment required.",
     404: "Resource not found.",
     405: "Operation not allowed.",
+    406: "Operation not accepted. Might be illeagal value."
     408: "Request timeout.",
     409: "Resource duplicated.",
+    412: "The transaction failed."
     423: "Client locked. Maximum number of attempts exceeded. Please try again later.",
     500: "Internal server error.",
 }
