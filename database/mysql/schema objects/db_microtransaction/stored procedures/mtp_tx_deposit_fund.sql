58e683c9e357ed3660e8e7439d21131e1bdd32e8
hyoungsookim <hskim@groceryheaven.com>
2018-11-16 21:48:24 -0800

    Update procedure to handle error

--- database/mysql/schema objects/db_microtransaction/stored procedures/mtp_tx_deposit_fund.sql
+++ database/mysql/schema objects/db_microtransaction/stored procedures/mtp_tx_deposit_fund.sql
@@ -16,6 +16,10 @@ main:begin
     declare $recipient_account_type smallint default null;
     declare $updated_at datetime default current_timestamp();
 
+	declare $data_not_found tinyint default false;
+	declare continue handler for 1329
+		set $data_not_found = true;
+
     declare exit handler for SQLEXCEPTION, SQLWARNING
     begin
         rollback;
@@ -26,18 +30,18 @@ main:begin
 
     set $sender_no = check_user($sender_id);
     if ($sender_no is null) then
-        set $error_code = 30400;            /* invalid request */
+        set $error_code = 400;          /* invalid request */
         leave main;
     end if; 
 
     if ($recipient_account_id is null or trim($recipient_account_id) = '') then
-		set $error_code = 30400;			/* invalid request */
+		set $error_code = 400;          /* invalid request */
 		leave main;
     end if;
 
     /* $deposit_amount should be bigger than 0.0 */
     if ($deposit_amount < 0.0) then 
-        set $error_code = 30400;            /* invalid request */
+        set $error_code = 406;          /* not accpeted. */
         leave main;
     end if;
 
@@ -46,24 +50,24 @@ main:begin
     from mtt_uw_user_accounts 
     where account_id = $recipient_account_id;
 
-    if ($recipient_account_no is null) then 
-        set $error_code = 30404;            /* resource not found */
+    if ($data_not_found = true or row_count() <= 0) then
+        set $error_code = 404;          /* resource not found */
         leave main;
     end if;
 
     /* 
-    account_type 0 means reward account.
     $deposit_type is trx_type of mtt_tx_transaction_types
+    account_type 0 means reward account.
     So $deposit_type 1200 means cash income and must go to account_type > 0 
     */
     if $recipient_account_type = 0 then 
         if $deposit_type <> 1100 then
-            set $error_code = 30400;        /* invalid request */
+            set $error_code = 400;        /* invalid request */
             leave main;
         end if;
     elseif $recipient_account_type > 0 then 
         if $deposit_type <> 1200 then 
-            set $error_code = 30400;        /* invalid request */
+            set $error_code = 400;        /* invalid request */
             leave main;
         end if;
     end if;
@@ -75,7 +79,7 @@ main:begin
         updated_at = $updated_at  
     where account_no = $recipient_account_no;
 
-    call mtp_tx_write_transaction
+    call mtp_tx_write_transaction_for_deposit
     (
         $recipient_account_no,
         $deposit_type,
