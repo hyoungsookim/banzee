8e1c433206eca95f6baf3834ee225c21af2db70d
hyoungsookim <hskim@groceryheaven.com>
2018-11-16 23:00:59 -0800

    Fix error

--- server/messages.py
+++ server/messages.py
@@ -5,18 +5,18 @@ Error message dictionary
 """
 
 messages = {
-    102: "The transaction is pending."
+    102: "The transaction is pending.",
     200: "The request was completed successfully.",
-    205: "The transaction is canceled."
+    205: "The transaction is canceled.",
     400: "Invalid request. Client might be missing some mandatory values or might call a wrong API.",
     401: "Authentication failed. Invalid request-header field or not acceptable client.",
     402: "Payment required.",
     404: "Resource not found.",
     405: "Operation not allowed.",
-    406: "Operation not accepted. Might be illeagal value."
+    406: "Operation not accepted. Might be illeagal value.",
     408: "Request timeout.",
     409: "Resource duplicated.",
-    412: "The transaction failed."
+    412: "The transaction failed.",
     423: "Client locked. Maximum number of attempts exceeded. Please try again later.",
     500: "Internal server error.",
 }
