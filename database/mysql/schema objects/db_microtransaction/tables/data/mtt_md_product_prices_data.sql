cbc0ae273305c854f25fd2d02437728c4c758fae
hyoungsookim <hskim@groceryheaven.com>
2019-01-09 22:10:39 +0900

    Add sample data

---
+++ database/mysql/schema objects/db_microtransaction/tables/data/mtt_md_product_prices_data.sql
@@ -0,0 +1,7 @@
+insert into mtt_md_product_prices 
+    (product_no, account_type, unit_price, updated_datetime)
+values
+    (7, 840, 15.5, now()),
+    (7, 0, 155, now()),
+    (8, 840, 23.9, now()),
+    (8, 0, 239, now());
